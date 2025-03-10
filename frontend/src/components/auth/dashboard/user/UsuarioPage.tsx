d8cea6509c00e4a9c8a15e7376044318667d9a01
Rudzygit <odruizs22@itp.edu.co>
2025-03-10 00:28:22 -0500

    reestablecimiento de rutas

--- frontend/src/components/auth/dashboard/user/UsuarioPage.tsx
+++ frontend/src/components/auth/dashboard/user/UsuarioPage.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import React from "react";
-import ProtegerRuta from "../../../../app/protegerRuta";
 import "./UsuarioPage.css";
 
 const UsuarioPage: React.FC = () => {
   return (
-    <ProtegerRuta roleRequired="usuario">
-      <div className="usuario-container">
-        <h1>Panel de Usuario</h1>
-        <p>Aquí puedes ver y agendar tus citas.</p>
-      </div>
-    </ProtegerRuta>
+    <div className="usuario-container">
+      <h1>Panel de Usuario</h1>
+      <p>Aquí puedes ver y agendar tus citas.</p>
+    </div>
   );
 };
 
