8e9a83310eebe3f6e50f9979f86733036b86cc49
Stiven-21 <stiven.cor2002@gmail.com>
2025-03-09 22:31:41 -0500

    Interfaz register

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
-  return <>Dashboard</>;
+  const { data: session, status } = useSession(); //Para obtener la sesion del usuario
+
+  if (status === "loading") return <p>Loading...</p>;
+
+  return <>{JSON.stringify(session)}</>;
 }
